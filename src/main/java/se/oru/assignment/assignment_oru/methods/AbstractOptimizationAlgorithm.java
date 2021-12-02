a8636439eb0ebde10f228c685081379ea28e53a8
PaoloForte95 <paolo.forte1995@gmail.com>
2021-12-02 17:03:44 +0200

    Added 2 new classes to initialize an optimization problem and an optimization algorithm

---
+++ src/main/java/se/oru/assignment/assignment_oru/methods/AbstractOptimizationAlgorithm.java
@@ -0,0 +1,83 @@
+package se.oru.assignment.assignment_oru.methods;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import java.util.TreeSet;
+import java.util.logging.Logger;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.lang.ArrayUtils;
+import org.metacsp.multi.spatioTemporal.paths.PoseSteering;
+import org.metacsp.multi.spatioTemporal.paths.TrajectoryEnvelope;
+import org.metacsp.multi.spatioTemporal.paths.TrajectoryEnvelope.SpatialEnvelope;
+import org.metacsp.utility.UI.Callback;
+import org.metacsp.utility.logging.MetaCSPLogging;
+
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Polygon;
+
+import aima.core.util.datastructure.Pair;
+import se.oru.assignment.assignment_oru.OptimizationProblem;
+import se.oru.assignment.assignment_oru.Task;
+import se.oru.coordination.coordination_oru.AbstractTrajectoryEnvelopeCoordinator;
+
+import se.oru.coordination.coordination_oru.CriticalSection;
+import se.oru.coordination.coordination_oru.IndexedDelay;
+import se.oru.coordination.coordination_oru.Mission;
+
+import se.oru.coordination.coordination_oru.RobotReport;
+import se.oru.coordination.coordination_oru.TrajectoryEnvelopeCoordinator;
+
+import se.oru.coordination.coordination_oru.fleetmasterinterface.AbstractFleetMasterInterface;
+import se.oru.coordination.coordination_oru.fleetmasterinterface.FleetMasterInterface;
+import se.oru.coordination.coordination_oru.fleetmasterinterface.FleetMasterInterfaceLib.CumulatedIndexedDelaysList;
+
+
+import se.oru.coordination.coordination_oru.motionplanning.AbstractMotionPlanner;
+import se.oru.coordination.coordination_oru.motionplanning.ompl.ReedsSheppCarPlanner;
+import se.oru.coordination.coordination_oru.motionplanning.ompl.ReedsSheppCarPlanner.PLANNING_ALGORITHM;
+import se.oru.coordination.coordination_oru.util.FleetVisualization;
+import se.oru.coordination.coordination_oru.util.Missions;
+import se.oru.coordination.coordination_oru.util.Robot;
+
+import com.google.ortools.linearsolver.*;
+
+
+
+public abstract class AbstractOptimizationAlgorithm {
+	 
+		
+	/** 
+	 * Solve the optimization problem given as input considering both B and F Functions. The objective function is defined as sum(c_ij * x_ij) for (i = 1...n)(j = 1...m).
+	 * with n = number of robot and m = number of tasks. The solver first finds the optimal solution considering only B function and then
+	 * for this each solution (that is an assignment) evaluates the cost of F function. Then a new optimal solution considering only B is 
+	 * computed and it is consider only if the cost of this new assignment considering only B is less than the min cost of previous assignments
+	 * considering both F and B function
+	 * @param optimizationProblem -> An optimization problem defined with {@link #createOptimizationProblem}
+	 * @param tec -> an Abstract Trajectory Envelope Coordinator
+	 * @param oap -> an optimization problem defined with {@link }
+
+	 * @return An Optimal Assignment that minimize the objective function
+	 */	
+	public abstract double [][][] solveOptimizationProblem(MPSolver optimizationProblem,AbstractTrajectoryEnvelopeCoordinator tec,OptimizationProblem oap);
+	
+	
+	
+	
+	}
+
