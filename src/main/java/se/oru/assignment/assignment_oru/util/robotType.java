064e22d5355f019551cf23f026a9cb50bdbbc753
PaoloForte95 <paolo.forte1995@gmail.com>
2021-12-29 18:40:05 +0100

    Added robotType enum class;

---
+++ src/main/java/se/oru/assignment/assignment_oru/util/robotType.java
@@ -0,0 +1,22 @@
+package se.oru.assignment.assignment_oru.util;
+
+/**
+ * This class is used to set the robotType
+ * @author paolodell
+ *
+ */
+
+public abstract class robotType {
+	
+	/**
+	 * Type of the robot
+	 * @author pofe
+	 *
+	 */
+	public static enum ROBOT_TYPE {MOBILE_ROBOT, FORKLIFT};
+	
+	
+}
+
+
+
