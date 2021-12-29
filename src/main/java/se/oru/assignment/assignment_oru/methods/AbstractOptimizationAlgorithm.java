e246593338864a7b3de630ee2b68dd2174e16073
PaoloForte95 <paolo.forte1995@gmail.com>
2021-12-29 18:42:34 +0100

    Added a systematic algorithm approach to solve an optimization problem;

--- src/main/java/se/oru/assignment/assignment_oru/methods/AbstractOptimizationAlgorithm.java
+++ src/main/java/se/oru/assignment/assignment_oru/methods/AbstractOptimizationAlgorithm.java
@@ -1,80 +1,61 @@
 package se.oru.assignment.assignment_oru.methods;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Random;
-import java.util.TreeSet;
-import java.util.logging.Logger;
 
-import org.apache.commons.beanutils.BeanUtils;
-import org.apache.commons.lang.ArrayUtils;
-import org.metacsp.multi.spatioTemporal.paths.PoseSteering;
-import org.metacsp.multi.spatioTemporal.paths.TrajectoryEnvelope;
-import org.metacsp.multi.spatioTemporal.paths.TrajectoryEnvelope.SpatialEnvelope;
-import org.metacsp.utility.UI.Callback;
-import org.metacsp.utility.logging.MetaCSPLogging;
-
-
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.Polygon;
-
-import aima.core.util.datastructure.Pair;
-import se.oru.assignment.assignment_oru.OptimizationProblem;
-import se.oru.assignment.assignment_oru.Task;
+import se.oru.assignment.assignment_oru.AbstractOptimizationProblem;
 import se.oru.coordination.coordination_oru.AbstractTrajectoryEnvelopeCoordinator;
 
-import se.oru.coordination.coordination_oru.CriticalSection;
-import se.oru.coordination.coordination_oru.Mission;
-
-import se.oru.coordination.coordination_oru.RobotReport;
-import se.oru.coordination.coordination_oru.TrajectoryEnvelopeCoordinator;
-
-
-
-
-import se.oru.coordination.coordination_oru.motionplanning.AbstractMotionPlanner;
-import se.oru.coordination.coordination_oru.motionplanning.ompl.ReedsSheppCarPlanner;
-import se.oru.coordination.coordination_oru.motionplanning.ompl.ReedsSheppCarPlanner.PLANNING_ALGORITHM;
-import se.oru.coordination.coordination_oru.util.FleetVisualization;
-import se.oru.coordination.coordination_oru.util.Missions;
-import se.oru.coordination.coordination_oru.util.Robot;
 
 import com.google.ortools.linearsolver.*;
 
+/**
+ * This class provides a method to compute an optimal task assignment for a fleet of robots and a set of tasks . 
+ * An instantiatable {@link AbstractOptimizationAlgorithm} must provide an implementation of the {@link #solveOptimizationProblem} function to compute the optimal solution
+ * @author pofe
+ *
+ */
 
+public abstract class AbstractOptimizationAlgorithm  {
+	
 
-public abstract class AbstractOptimizationAlgorithm {
-	 
+	protected double timeOut = Double.POSITIVE_INFINITY;
+	
+	
+	
+	/**
+	 * Set the timeOut for the optimization algorithm (in minutes). The algorithm will search a solution until this time.
+	 * Use number from 0.1 (10 seconds) to 0.6 (60 seconds) for seconds
+	 * @param minutes -> timeout value in minutes
+	 * 
+	 */
+	public void setTimeOutinMinutes(double minutes) {
+		if(timeOut < 0) {
+			throw new Error("Timeout cannot be negative!");
+		}
+		if(timeOut < 0.6) {
+			this.timeOut = minutes*1000;
+		}
+		else {
+			this.timeOut = minutes*60*1000;
+		}
 		
+	}
+	
+	
+	/**
+	 * Get the timeout for this optimization algorithm.
+	 * @return The timeout set in minutes. 
+	 */
+	
+	public double getTimeout() {
+		return this.timeOut;
+	}
+	
+	
+	
 	/** 
-	 * Solve the optimization problem given as input considering both B and F Functions. The objective function is defined as sum(c_ij * x_ij) for (i = 1...n)(j = 1...m).
-	 * with n = number of robot and m = number of tasks. The solver first finds the optimal solution considering only B function and then
-	 * for this each solution (that is an assignment) evaluates the cost of F function. Then a new optimal solution considering only B is 
-	 * computed and it is consider only if the cost of this new assignment considering only B is less than the min cost of previous assignments
-	 * considering both F and B function
-	 * @param optimizationProblem -> An optimization problem defined with {@link #createOptimizationProblem}
-	 * @param tec -> an Abstract Trajectory Envelope Coordinator
-	 * @param oap -> an optimization problem defined with {@link }
-
-	 * @return An Optimal Assignment that minimize the objective function
+	 * Solve the optimization problem given as input
+	 * @param oap -> An optimization problem defined with {@link #buildOptimizationProblem}
+	 * @return The optimal Assignment
 	 */	
-	public abstract double [][][] solveOptimizationProblem(MPSolver optimizationProblem,AbstractTrajectoryEnvelopeCoordinator tec,OptimizationProblem oap);
-	
-	
-	
-	
+	public abstract double [][][] solveOptimizationProblem(AbstractOptimizationProblem oap);	
 	}
 
--- src/main/java/se/oru/assignment/assignment_oru/methods/SystematicAlgorithm.java
+++ src/main/java/se/oru/assignment/assignment_oru/methods/SystematicAlgorithm.java
@@ -1,79 +1,42 @@
 package se.oru.assignment.assignment_oru.methods;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.lang.reflect.InvocationTargetException;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Random;
-import java.util.TreeSet;
-import java.util.logging.Logger;
-
-import org.apache.commons.beanutils.BeanUtils;
-import org.apache.commons.lang.ArrayUtils;
-import org.metacsp.multi.spatioTemporal.paths.PoseSteering;
-import org.metacsp.multi.spatioTemporal.paths.TrajectoryEnvelope;
-import org.metacsp.multi.spatioTemporal.paths.TrajectoryEnvelope.SpatialEnvelope;
-import org.metacsp.utility.UI.Callback;
-import org.metacsp.utility.logging.MetaCSPLogging;
-
-
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.Polygon;
-
-import aima.core.util.datastructure.Pair;
 import se.oru.assignment.assignment_oru.OptimizationProblem;
-import se.oru.assignment.assignment_oru.Task;
-import se.oru.coordination.coordination_oru.AbstractTrajectoryEnvelopeCoordinator;
-
-import se.oru.coordination.coordination_oru.CriticalSection;
-import se.oru.assignment.assignment_oru.IndexedDelay;
-import se.oru.coordination.coordination_oru.Mission;
-
-import se.oru.coordination.coordination_oru.RobotReport;
-import se.oru.coordination.coordination_oru.TrajectoryEnvelopeCoordinator;
-
-import se.oru.assignment.assignment_oru.fleetmasterinterface.AbstractFleetMasterInterface;
-import se.oru.assignment.assignment_oru.fleetmasterinterface.FleetMasterInterface;
-import se.oru.assignment.assignment_oru.fleetmasterinterface.FleetMasterInterfaceLib.CumulatedIndexedDelaysList;
-
-
-import se.oru.coordination.coordination_oru.motionplanning.AbstractMotionPlanner;
-import se.oru.coordination.coordination_oru.motionplanning.ompl.ReedsSheppCarPlanner;
-import se.oru.coordination.coordination_oru.motionplanning.ompl.ReedsSheppCarPlanner.PLANNING_ALGORITHM;
-import se.oru.coordination.coordination_oru.util.FleetVisualization;
-import se.oru.coordination.coordination_oru.util.Missions;
-import se.oru.coordination.coordination_oru.util.Robot;
-
+import se.oru.assignment.assignment_oru.AbstractOptimizationProblem;
 import com.google.ortools.linearsolver.*;
 
+/**
+ * This class provide a systematic algorithm to solve an optimization problem defined with the class {@link se.oru.assignment.assignment_oru.AbstractOptimizationProblem#buildOptimizationProblem}.
+ * 
+ * @author pofe
+ *
+ */
 
-
-public class SystematicAlgorithm extends AbstractOptimizationAlgorithm {
+public class SystematicAlgorithm extends AbstractOptimizationAlgorithm{
 	 
 	
 	
+	/** 
+	 * Solve the optimization problem given as input using a systematic algorithm. The objective function is defined as alpha*B + (1-alpha)*F.
+	 * The solver first finds the optimal solution considering only B function and then
+	 * for each solution (i.e. a sub-optimal assignment) evaluates the cost of F function. Each new sub-optimal solution (i.e. with the cost less that then previous sub-optimal one)
+	 * introduces a new constraint on cost for the next solutions. This constraint prunes all solutions that have a cost considering only B higher that the sub-optimal(that consider B + F) 
+	 * since none of these solutions can be the optimal one.
+	 * @param oap -> an optimization problem defined with {@link se.oru.assignment.assignment_oru.AbstractOptimizationProblem#buildOptimizationProblem}
+
+	 * @return An Optimal Assignment that minimize the objective function
+	 */	
 	
-	
-	public double [][][] solveOptimizationProblem(MPSolver optimizationProblem,AbstractTrajectoryEnvelopeCoordinator tec,OptimizationProblem oap){
-		
+	public double [][][] solveOptimizationProblem(AbstractOptimizationProblem oap){
+		MPSolver optimizationModel = oap.getModel();
 		ArrayList<Integer> IDsAllRobots = oap.getRobotsIDs();
 		ArrayList<Integer> IDsAllTasks = oap.getTasksIDs();
 		
 		int numRobotAug = IDsAllRobots.size();
 		int numTaskAug = IDsAllTasks.size();
 		int maxNumPaths = oap.getmaxNumberOfAlternativePaths();
-		double [][][] costValuesMatrix = oap.getCostValuesMatrix();
+		double [][][] costValuesMatrix = oap.getInterferenceFreeCostMatrix();
 		
 		double linearWeight = oap.getLinearWeight();
 	
@@ -88,21 +51,20 @@ public class SystematicAlgorithm extends AbstractOptimizationAlgorithm {
 		double costFOptimal = 0;
 		
 		//Solve the optimization problem
-
 		
 		
-		MPSolver.ResultStatus resultStatus = optimizationProblem.solve();
+		MPSolver.ResultStatus resultStatus = optimizationModel.solve();
 		long timeOffsetInitial = Calendar.getInstance().getTimeInMillis();
 		long timeOffset = 0;
 		int cont = 1;
-		while(resultStatus != MPSolver.ResultStatus.INFEASIBLE && timeOffset < oap.getTimeout()) {
+		while(resultStatus != MPSolver.ResultStatus.INFEASIBLE && timeOffset < getTimeout()) {
 			//Evaluate an optimal assignment that minimize only the B function
 			timeProva2 = Calendar.getInstance().getTimeInMillis();
-			resultStatus = optimizationProblem.solve();
+			resultStatus = optimizationModel.solve();
 			timeProvaFinal2 = Calendar.getInstance().getTimeInMillis();
 			long timeRequiredProva =  timeProvaFinal2-  timeProva2;
 			//Evaluate the Assignment Matrix
-			double [][][] AssignmentMatrix = oap.saveAssignmentMatrix(numRobotAug,numTaskAug,optimizationProblem);
+			double [][][] AssignmentMatrix = oap.getAssignmentMatrix(optimizationModel);
 			//Initialize cost of objective value
 			double costofAssignment = 0;
 			double costofAssignmentForConstraint = 0;
@@ -121,9 +83,9 @@ public class SystematicAlgorithm extends AbstractOptimizationAlgorithm {
 							
 							if (linearWeight != 1) {
 								//Evaluate cost of F function only if alpha is not equal to 1
-								double costB = optimizationProblem.objective().getCoefficient(optimizationProblem.variables()[i*numTaskAug*maxNumPaths+j*maxNumPaths+s]);
+								double costB = optimizationModel.objective().getCoefficient(optimizationModel.variables()[i*numTaskAug*maxNumPaths+j*maxNumPaths+s]);
 								//fileStream11.println(" ");
-								costF = oap.evaluatePathDelay(robotID,taskID,s,AssignmentMatrix,tec)/oap.getSumArrivalTime();
+								costF = oap.evaluateInterferenceCost(robotID,taskID,s,AssignmentMatrix);
 								costofAssignment = linearWeight*costB + (1-linearWeight)*costF + costofAssignment ;
 								costofAssignmentForConstraint = costValuesMatrix[i][j][s] + costF + costofAssignmentForConstraint;
 								
@@ -133,7 +95,7 @@ public class SystematicAlgorithm extends AbstractOptimizationAlgorithm {
 								
 							}
 							else {
-								double costB = optimizationProblem.objective().getCoefficient(optimizationProblem.variables()[i*numTaskAug*maxNumPaths+j*maxNumPaths+s]);
+								double costB = optimizationModel.objective().getCoefficient(optimizationModel.variables()[i*numTaskAug*maxNumPaths+j*maxNumPaths+s]);
 								costofAssignment = Math.pow(linearWeight*costB, 2) + costofAssignment ;
 								costofAssignmentForConstraint = costValuesMatrix[i][j][s]  + costofAssignmentForConstraint;
 							}
@@ -156,15 +118,15 @@ public class SystematicAlgorithm extends AbstractOptimizationAlgorithm {
 						}
 					}
 				}
-				costBOptimal = optimizationProblem.objective().value();
+				costBOptimal = optimizationModel.objective().value();
 				costFOptimal =  costofAssignmentForConstraint- costBOptimal;
 			}
 			
 			//Add the constraint on cost for next solution
 			//add +0,005 in order for tolerance
-			//optimizationProblem = oap.constraintOnCostSolution(optimizationProblem,costofAssignmentForConstraint);
+			optimizationModel = oap.constraintOnCostSolution(optimizationModel,costofAssignmentForConstraint);
 			//Add the constraint to actual solution in order to consider this solution as already found  
-			optimizationProblem = oap.constraintOnPreviousSolution(optimizationProblem,AssignmentMatrix);
+			optimizationModel = oap.constraintOnPreviousSolution(optimizationModel,AssignmentMatrix);
 			long timeOffsetFinal = Calendar.getInstance().getTimeInMillis();
 			timeOffset = timeOffsetFinal - timeOffsetInitial;
 			cont +=1;			
@@ -188,12 +150,11 @@ public class SystematicAlgorithm extends AbstractOptimizationAlgorithm {
 	 * computed and it is consider only if the cost of this new assignment considering only B is less than the min cost of previous assignments
 	 * considering both F and B function
 	 * @param optimizationProblem -> An optimization problem defined with {@link #createOptimizationProblem}
-	 * @param tec -> an Abstract Trajectory Envelope Coordinator
 	 * @param oap -> an optimization problem defined with {@link }
 
 	 * @return An Optimal Assignment that minimize the objective function
 	 */	
-	public double [][][] solveOptimizationProblem2(MPSolver optimizationProblem,AbstractTrajectoryEnvelopeCoordinator tec,OptimizationProblem oap){
+	public double [][][] solveOptimizationProblem2(MPSolver optimizationProblem,OptimizationProblem oap){
 		
 
 		ArrayList<Integer> IDsAllRobots = oap.getRobotsIDs();
@@ -202,7 +163,7 @@ public class SystematicAlgorithm extends AbstractOptimizationAlgorithm {
 		int numRobotAug = IDsAllRobots.size();
 		int numTaskAug = IDsAllTasks.size();
 		int maxNumPaths = oap.getmaxNumberOfAlternativePaths();
-		double [][][] costValuesMatrix = oap.getCostValuesMatrix();
+		double [][][] costValuesMatrix = oap.getInterferenceFreeCostMatrix();
 		
 		double linearWeight = oap.getLinearWeight();
 		
@@ -219,9 +180,9 @@ public class SystematicAlgorithm extends AbstractOptimizationAlgorithm {
 		long timeOffsetInitial = Calendar.getInstance().getTimeInMillis();
 		long timeOffset = 0;
 		int cont = 1;
-		while(resultStatus != MPSolver.ResultStatus.INFEASIBLE && timeOffset < oap.getTimeout()) {
+		while(resultStatus != MPSolver.ResultStatus.INFEASIBLE && timeOffset < getTimeout()) {
 			//Evaluate the Assignment Matrix
-			double [][][] AssignmentMatrix = oap.saveAssignmentMatrix(numRobotAug,numTaskAug,optimizationProblem);
+			double [][][] AssignmentMatrix = oap.getAssignmentMatrix(optimizationProblem);
 			//Initialize cost of objective value
 			double costofAssignment = 0;
 			double costofAssignmentForConstraint = 0;
@@ -241,7 +202,7 @@ public class SystematicAlgorithm extends AbstractOptimizationAlgorithm {
 								//Evaluate cost of F function only if alpha is not equal to 1
 								double costB = optimizationProblem.objective().getCoefficient(optimizationProblem.variables()[i*numTaskAug*maxNumPaths+j*maxNumPaths+s]);
 								//fileStream11.println(" ");
-								costF = oap.evaluatePathDelay(robotID,taskID,s,AssignmentMatrix,tec)/oap.getSumArrivalTime();
+								costF = oap.evaluateInterferenceCost(robotID,taskID,s,AssignmentMatrix);
 								costofAssignment = linearWeight*costB + (1-linearWeight)*costF + costofAssignment ;
 								costofAssignmentForConstraint = costValuesMatrix[i][j][s] + costF + costofAssignmentForConstraint;
 								
@@ -294,6 +255,13 @@ public class SystematicAlgorithm extends AbstractOptimizationAlgorithm {
 		
 		return  optimalAssignmentMatrix;    
 	}
+
+
+
+	
+
+
+
 
 	
 	
